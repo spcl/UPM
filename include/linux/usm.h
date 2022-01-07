fe181ee4c54143192a668376f67c82da0e396f42
QW <facelessq@icloud.com>
2022-01-07 10:21:52 +0100

    update flags

--- include/linux/usm.h
+++ include/linux/usm.h
@@ -4,6 +4,7 @@
 int usm_madvise(struct vm_area_struct *vma, unsigned long start,
 		unsigned long end, int advice, unsigned long *vm_flags);
 
-void usm_exit(struct mm_struct *mm);
+// void usm_exit(struct mm_struct *mm);
+void usm_exit(int pid, struct mm_struct *mm);
 
-#endif /* __LINUX_KSM_H */
+#endif /* __LINUX_USM_H */
